34b9172ce063ea47133daedd01b13dce8038c7f1
alextebbitt <alex.tebbitt87@gmail.com>
2022-07-03 19:05:36 +0200

    spinner problem fixed, now post id problem

--- src/components/Home/Posts/Post.jsx
+++ src/components/Home/Posts/Post.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
+import { Link } from "react-router-dom";
 
 const Post = () => {
     const { posts } = useSelector((state) => state.posts)
@@ -8,8 +9,10 @@ const Post = () => {
     {
         return (
             <div key={post.id}>
+                <Link to={"/posts/" + post.id}>
                 <h2>{post.title}</h2>
-                <span> {post.body} </span>
+                </Link>
+                {/* <span> {post.body} </span> */}
             </div>
         )
     })
---
+++ src/components/PostDetail/PostDetail.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+const PostDetail = () => {
+    const { id } = useParams();
+    console.log(id);
+  return (
+    <div>PostDetail</div>
+  )
+}
+
+export default PostDetail
